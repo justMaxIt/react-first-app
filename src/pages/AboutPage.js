dbbd9b6ce66f36cd41aa6207973b1d1bfa31a477
justMaxIt <maximfedor2015@gmail.com>
2020-08-19 17:17:56 +0300

    useEffect

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AboutMenu from "../components/AboutMenu";
 import AboutContent from "../components/AboutContent";
 import AboutModal from "../components/AboutModal";
@@ -6,6 +6,15 @@ import AboutModal from "../components/AboutModal";
 function AboutPage() {
   const [isVisable, setIsVisable] = useState(false);
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    if (isVisable) {
+      return () => setCounter(0);
+    }
+    if (counter >= 9) {
+      return () => setIsVisable(true);
+    }
+  }, [counter, isVisable]);
 
   return (
     <div className="about-page">
